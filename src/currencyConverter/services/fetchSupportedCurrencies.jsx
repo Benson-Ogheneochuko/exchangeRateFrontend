3794da8298466d03c36284b2cc3ed1541923f2ce
Nockk <ob.ogheneochuko@gmail.com>
2024-08-18 14:54:47 +0100

    configure vite env

--- src/currencyConverter/services/fetchSupportedCurrencies.jsx
+++ src/currencyConverter/services/fetchSupportedCurrencies.jsx
@@ -1,10 +1,9 @@
-const currencyEndpoint =
-  // process.env.CurrencyOptionsEndpoint ||
-  "http://localhost:8000/v1/api/converter/supported-currencies";
+const apiEndpoint = `${import.meta.env.VITE_BASE_API_URL}${import.meta.env.VITE_CURRENCIES_ENDPOINT}`
+  || import.meta.env.VITE_LOCAL_HOST_CURRENCIES_ENDPOINT
 
 export const fetchSupportedCurrencies = async () => {
   try {
-    const response = await fetch(currencyEndpoint);
+    const response = await fetch(apiEndpoint);
     if (!response.ok) {
       console.error("Failed to fetch currency options:", response);
       return [];
